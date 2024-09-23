bcb2f30bf5dc9bd65b6e9471a5ecfd90f1380f72
Lucas Rodrigues <101301943+httplucas@users.noreply.github.com>
2024-09-23 19:38:03 -0300

    Delete QRcode_project directory

--- QRcode_project/main.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-qrcode_usuario = input('Bem vindo ao gerador de QRcode, qual site você deseja gerar em QR? ')
-img = qrcode.make(f'https://{qrcode_usuario}.com')
-
-img.save('qrcode_1.png')
-
-
-
-
